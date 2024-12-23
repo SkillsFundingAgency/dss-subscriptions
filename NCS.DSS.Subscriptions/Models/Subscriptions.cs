38366dcffe4ab989737bab1ef0e6c2b6a5fd01f9
Pradeep Kumar Voorukonda <PradeepKumar.VOORUKONDA@EDUCATION.GOV.UK>
2024-12-23 12:12:27 +0000

    json ignore for some fields

--- NCS.DSS.Subscriptions/Models/Subscriptions.cs
+++ NCS.DSS.Subscriptions/Models/Subscriptions.cs
@@ -1,5 +1,6 @@
 ﻿using DFC.Swagger.Standard.Annotations;
 using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
 
 namespace NCS.DSS.Subscriptions.Models
 {
@@ -24,10 +25,12 @@ namespace NCS.DSS.Subscriptions.Models
         [Example(Description = "true")]
         public bool? Subscribe { get; set; }
 
+        [JsonIgnore]
         [Display(Description = "Last modified date & time")]
         [Example(Description = "2018-06-21T14:45:00")]
         public DateTime? LastModifiedDate { get; set; }
 
+        [JsonIgnore]
         [StringLength(10, MinimumLength = 10)]
         [RegularExpression(@"^[0-9]+$")]
         [Display(Description = "Identifier of the touchpoint who made the last change to the record")]
